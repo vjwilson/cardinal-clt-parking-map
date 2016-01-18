e9089d79832b43407e9e689c5d25121235b0908d
Eric Allen <decoy@internetalche.me>
2016-01-18 15:00:50 -0500

    added resetting center on window resize

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,14 +1,23 @@
 (function() {
 
+  var center;
+  var map;
+
   // When the window has finished loading create our google map below
   google.maps.event.addDomListener(window, 'load', init);
 
+  google.maps.event.addDomListener(window, 'resize', function() {
+    map.setCenter(center);
+  });
+
   function init() {
+    center = new google.maps.LatLng(35.227156, -80.846351);
+
     // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
     var mapOptions = {
       zoom: 16,
       scrollwheel: false,
-      center: new google.maps.LatLng(35.227156, -80.846351),
+      center: center,
       //generated from Snazzy Maps.
       styles: [{"featureType":"landscape.man_made","elementType":"geometry.fill","stylers":[{"color":"#e9e5dc"}]},{"featureType":"landscape.natural","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#b8cb93"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"poi.business","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"poi.medical","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"poi.park","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"poi.park","elementType":"geometry.fill","stylers":[{"color":"#ccdca1"}]},{"featureType":"poi.sports_complex","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"road","elementType":"geometry.fill","stylers":[{"hue":"#ff0000"},{"saturation":-100},{"lightness":99}]},{"featureType":"road","elementType":"geometry.stroke","stylers":[{"color":"#808080"},{"lightness":54},{"visibility":"off"}]},{"featureType":"road","elementType":"labels.text.fill","stylers":[{"color":"#767676"}]},{"featureType":"road","elementType":"labels.text.stroke","stylers":[{"color":"#ffffff"}]},{"featureType":"water","elementType":"all","stylers":[{"saturation":43},{"lightness":-11},{"color":"#1a85c7"}]}]
     };
@@ -18,7 +27,7 @@
     var mapElement = document.getElementById('map');
 
     // Create the Google Map using our element and options defined above
-    var map = new google.maps.Map(mapElement, mapOptions);
+    map = new google.maps.Map(mapElement, mapOptions);
 
     // Let's also add a marker while we're at it
     var cardinalLogo = './assets/images/cardinal-logo.svg';
