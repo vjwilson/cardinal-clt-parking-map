8ed004591dace8e80e6931c9055d7ae4a92bb862
Eric Allen <decoy@internetalche.me>
2016-01-18 14:57:29 -0500

    Updated some basic styling for map and edited the markup

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -4,17 +4,12 @@
   google.maps.event.addDomListener(window, 'load', init);
 
   function init() {
-    // Basic options for a simple Google Map
     // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
     var mapOptions = {
-      // How zoomed in you want the map to start at (always required)
       zoom: 16,
-
-      // The latitude and longitude to center the map (always required)
-      center: new google.maps.LatLng(35.227156, -80.846351), // Parkard Place Charlotte
-
-      // How you would like to style the map.
-      // This is where you would paste any style found on Snazzy Maps.
+      scrollwheel: false,
+      center: new google.maps.LatLng(35.227156, -80.846351),
+      //generated from Snazzy Maps.
       styles: [{"featureType":"landscape.man_made","elementType":"geometry.fill","stylers":[{"color":"#e9e5dc"}]},{"featureType":"landscape.natural","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#b8cb93"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"poi.business","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"poi.medical","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"poi.park","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"poi.park","elementType":"geometry.fill","stylers":[{"color":"#ccdca1"}]},{"featureType":"poi.sports_complex","elementType":"all","stylers":[{"visibility":"on"}]},{"featureType":"road","elementType":"geometry.fill","stylers":[{"hue":"#ff0000"},{"saturation":-100},{"lightness":99}]},{"featureType":"road","elementType":"geometry.stroke","stylers":[{"color":"#808080"},{"lightness":54},{"visibility":"off"}]},{"featureType":"road","elementType":"labels.text.fill","stylers":[{"color":"#767676"}]},{"featureType":"road","elementType":"labels.text.stroke","stylers":[{"color":"#ffffff"}]},{"featureType":"water","elementType":"all","stylers":[{"saturation":43},{"lightness":-11},{"color":"#1a85c7"}]}]
     };
 
@@ -26,7 +21,7 @@
     var map = new google.maps.Map(mapElement, mapOptions);
 
     // Let's also add a marker while we're at it
-    var cardinalLogo = 'images/cardinal-logo.svg';
+    var cardinalLogo = './assets/images/cardinal-logo.svg';
 
     var officeMarker = new google.maps.Marker({
       position: new google.maps.LatLng(35.227156, -80.846351),
